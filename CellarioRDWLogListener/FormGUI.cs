2b9a7c49886410607416c504491bd8e47ac83505
santkumar <sant.kumar@bsse.ethz.ch>
2025-05-15 00:32:42 +0200

    Modified log file reading to two steps: 1. Timer event catching "Executing method" eveny 2 seconds. 2. Run logFileWatcher once the first step catches.
    TODO: In case of back to back read operation, wrong folder could be renamed.

--- CellarioRDWLogListener/FormGUI.cs
+++ CellarioRDWLogListener/FormGUI.cs
@@ -16,6 +16,13 @@ namespace CellarioRDWLogListener
     {
         private FileSystemWatcher logFileWatcher;
         private FileSystemWatcher newFileDetector;
+        private System.Threading.Timer logCheckTimer;
+        private bool isRunning = false;
+        private string logFilePath;
+        private string resultsPathParent;
+        private string barcode;
+        private string orderID;
+
         private DateTime lastUpdateTime = DateTime.MinValue;
         private readonly TimeSpan debounceDelay = TimeSpan.FromSeconds(1);
 
@@ -44,107 +51,156 @@ namespace CellarioRDWLogListener
             }
         }
 
-        private void btnStopListening(object sender, EventArgs e)
-        {
-            logFileWatcher?.Dispose();
-            btnStart.Enabled = true;
-            lblStatus.ForeColor = Color.Black;
-            lblStatus.Text = "Idle        ";
-            DisplayOnConsole("Stopped listening.");
-        }
-
         private void btnStartListening(object sender, EventArgs e)
         {
-            string logPath = txtLogFile.Text;
-            string resultsPath = txtResultsFolder.Text;
+            logFilePath = txtLogFile.Text;
+            resultsPathParent = txtResultsFolder.Text;
 
-            if (!File.Exists(logPath) || !Directory.Exists(resultsPath))
+            if (!File.Exists(logFilePath) || !Directory.Exists(resultsPathParent))
             {
                 MessageBox.Show("Please provide valid paths.");
                 return;
             }
 
+            DisplayOnConsole($"Now listening: {logFilePath}");
+            isRunning = true;
+
             btnStart.Enabled = false;
-            StartListening(logPath, resultsPath);
-            lblStatus.ForeColor = Color.Red;
+            btnStart.BackColor = Color.Gray;
+            btnStop.Enabled = true;
+            btnStop.BackColor = Color.Red;
+
+            lblStatus.ForeColor = Color.White;
+            lblStatus.BackColor = Color.Green;
             lblStatus.Text = "Running";
+
+            logCheckTimer = new System.Threading.Timer(
+                callback: _ => CheckForReadOperation(),
+                state: null,
+                dueTime: TimeSpan.Zero,
+                period: TimeSpan.FromSeconds(2));
         }
 
-        private void StartListening(string logPath, string resultsPath)
+        private void btnStopListening(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                return;
+            }
+
+            isRunning = false;
+
+            btnStart.Enabled = true;
+            btnStart.BackColor = Color.Green;
+            btnStop.Enabled = false;
+            btnStop.BackColor = Color.Gray;
+
+            lblStatus.ForeColor = Color.Black;
+            lblStatus.BackColor = Color.White;
+            lblStatus.Text = "Idle        ";
+
+            logFileWatcher?.Dispose();
+            logCheckTimer?.Dispose();
+            DisplayOnConsole("Stopped listening.");
+        }
+
+        private void CheckForReadOperation()
+        {
+            try
+            {
+                Invoke(new Action(() =>
+                {
+                    var lines = ReadAllLinesSafe(logFilePath);
+                    string lastLine = lines.LastOrDefault();
+
+                    if (lastLine != null && lastLine.Contains("Executing method"))
+                    {
+                        DisplayOnConsole("Read operation (executing) detected.");
+
+                        for (int i = lines.Count - 1; i >= 0; i--)
+                        {
+                            if (lines[i].Contains("Launching Command - 'Read'", StringComparison.OrdinalIgnoreCase))
+                            {
+                                barcode = ExtractBarcode(lines[i]);
+                                barcode = barcode.Replace(",", "");
+                                DisplayOnConsole($"Barcode: {barcode}");
+                                break;
+                            }
+                        }
+
+                        StartListeningForChanges();
+                        logCheckTimer?.Dispose();
+                    }
+                }));
+            }
+            catch (ObjectDisposedException) { /* Ignore on close */ }
+            catch (Exception ex)
+            {
+                DisplayOnConsole($"Error (Check For Read Operation): {ex.Message}");
+            }
+        }
+
+        private void StartListeningForChanges()
         {
             // Remove the watcher for old log file (new log file is created every day)
             logFileWatcher?.Dispose();
 
-            DisplayOnConsole($"Now listening: {logPath}");
+            DisplayOnConsole("Waiting for read operation to finish...");
 
             // TODO: Just being extra careful with LastWrite and Size...maybe its overkill?
             logFileWatcher = new FileSystemWatcher
             {
-                Path = Path.GetDirectoryName(logPath),
-                Filter = Path.GetFileName(logPath),
+                Path = Path.GetDirectoryName(logFilePath),
+                Filter = Path.GetFileName(logFilePath),
                 NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size,
             };
-            logFileWatcher.Changed += (s, e) => OnLogChanged(e.FullPath, resultsPath);
+            logFileWatcher.Changed += (s, e) => OnLogChanged();
             logFileWatcher.EnableRaisingEvents = true;
         }
 
-        private void OnLogChanged(string logPath, string parentResultsFolder)
+        private void OnLogChanged()
         {
-            DateTime rightNow = DateTime.Now;
-            if ((rightNow - lastUpdateTime) < debounceDelay) // skip
-            {
-                return;
-            }
-            lastUpdateTime = rightNow;
-
-            Thread.Sleep(500); // TODO: Allow RDW to finish writing...don't know if this is too little or too much?
+            logFileWatcher?.Dispose();
+            Thread.Sleep(2000);
 
             try
             {
-                string[] lines = File.ReadAllLines(logPath);
-                string lastLine = lines.LastOrDefault();
-                string barcode = null;
-
-                if (lastLine != null && lastLine.Contains("Read - Command Complete finished"))
+                if (!string.IsNullOrEmpty(barcode))
                 {
-                    for (int i = lines.Length - 1; i >= 0; i--)
+                    string latestFolder = GetLatestResultFolder(resultsPathParent);
+                    if (latestFolder != null)
                     {
-                        if (lines[i].Contains("Launching Command - 'Read'", StringComparison.OrdinalIgnoreCase))
+                        string originalName = Path.GetFileName(latestFolder);
+                        DisplayOnConsole($"Latest read results folder: {originalName}");
+                        string newFolderName = $"{barcode}_{originalName}";
+                        string newPath = Path.Combine(resultsPathParent, newFolderName);
+
+                        if (!Directory.Exists(newPath))
                         {
-                            DisplayOnConsole("Read operation completion detected.");
-                            barcode = ExtractBarcode(lines[i]);
-                            barcode = barcode.Replace(",", "");
-                            DisplayOnConsole($"Barcode: {barcode}");
-                            break;
+                            Directory.Move(latestFolder, newPath);
+                            DisplayOnConsole($"Renamed: {newFolderName}");
                         }
                     }
-
-                    if (!string.IsNullOrEmpty(barcode))
+                    else
                     {
-                        string latestFolder = GetLatestResultFolder(parentResultsFolder);
-                        if (latestFolder != null)
-                        {
-                            string originalName = Path.GetFileName(latestFolder);
-                            DisplayOnConsole($"Latest results folder: {originalName}");
-                            string newFolderName = $"{barcode}_{originalName}";
-                            string newPath = Path.Combine(parentResultsFolder, newFolderName);
-
-                            if (!Directory.Exists(newPath))
-                            {
-                                Directory.Move(latestFolder, newPath);
-                                DisplayOnConsole($"Renamed: {newFolderName}");
-                            }
-                        }
-                        else
-                        {
-                            DisplayOnConsole("No recent result folder found.");
-                        }
+                        DisplayOnConsole("No recent results folder found.");
                     }
                 }
+                else
+                {
+                    DisplayOnConsole("No barcode found, so no renaming of results folder.");
+                }
+
+                DisplayOnConsole($"Now listening: {logFilePath}");
+                logCheckTimer = new System.Threading.Timer(
+                    callback: _ => CheckForReadOperation(),
+                    state: null,
+                    dueTime: TimeSpan.Zero,
+                    period: TimeSpan.FromSeconds(2));
             }
             catch (Exception ex)
             {
-                DisplayOnConsole("Error: " + ex.Message);
+                DisplayOnConsole("Error (On Log Changed): " + ex.Message);
             }
         }
 
@@ -177,6 +233,27 @@ namespace CellarioRDWLogListener
         private void clearConsole_Click(object sender, EventArgs e)
         {
             textConsole.Clear();
+        }
+
+        private List<string> ReadAllLinesSafe(string logPath)
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                using (var fs = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        lines.Add(sr.ReadLine());
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                DisplayOnConsole($"Error (File Assist): {ex.Message}");
+            }
+            return lines;
         }
     }
 }
